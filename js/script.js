0801b9f88983317a4a65fe236f97f2d0b2b0c157
Aswar <63719871+Muh-Aswar@users.noreply.github.com>
2023-11-01 05:02:05 +0800

    Update Layout Crew
    Mengubah layout crew menjadi slider dan mengatur responsive di sisi mobile

--- js/script.js
+++ js/script.js
@@ -14,3 +14,18 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+// SLIDER PROFIL
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
